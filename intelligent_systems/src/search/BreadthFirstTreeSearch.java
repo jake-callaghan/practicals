8e0cb5d292dd9d0b8aa2c3ce8426ba22c9b3abd4
Jake Callaghan <jake.e.callaghan@gmail.com>
2015-10-22 00:37:42 +0100

    breadth-first tree search optimised

--- intelligent_systems/src/search/BreadthFirstTreeSearch.java
+++ intelligent_systems/src/search/BreadthFirstTreeSearch.java
@@ -4,18 +4,28 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Set;
 
+// -------------------------------------------------
+// edit: goalTest moved to when expanding the state, 
+// thus reducing time complexity by a factor b
+// -------------------------------------------------
+
 public class BreadthFirstTreeSearch {
 	public static Node findSolution(State initialConfiguration, GoalTest goalTest) {
 		Queue<Node> fifoQueue = new LinkedList<Node>();
 		fifoQueue.add(new Node(null, null, initialConfiguration));
 		while (!fifoQueue.isEmpty()) {
 			Node node = fifoQueue.remove();
-			if (goalTest.isGoal(node.state))
-				return node;
-			else {
+			//if (goalTest.isGoal(node.state))
+			//	return node;
+			//else {
 				for (Action action : node.state.getApplicableActions()) {
 					State newState = node.state.getActionResult(action);
-					fifoQueue.add(new Node(node, action, newState));
+					Node newNode = new Node(node, action, newState);
+					if (goalTest.isGoal(newState)) {
+						return newNode;
+					} else {
+						fifoQueue.add(newNode);
+					}
 				}
 			}
 		}
