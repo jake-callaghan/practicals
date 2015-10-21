9ec794469357c228cf8a58f58d5f53e39327699a
Jake Callaghan <ug13jc@tr02.tcs.cs.ox.ac.uk>
2015-10-21 18:21:48 +0100

    task (1a) complete: determined plaintext length

--- security/prac1/BeastAttack.java
+++ security/prac1/BeastAttack.java
@@ -7,22 +7,63 @@ public class BeastAttack
 
     public static void main(String[] args) throws Exception
     {
-	byte[] ciphertext=new byte[1024]; // will be plenty big enough
+		byte[] ciphertext=new byte[1024]; // will be plenty big enough
+		byte[] prefix = new byte[8];
+		
+		//===================//
+		//		 Task 1      //
+		//===================//
 
-	// e.g. this prints out the result of an encryption with no prefix	
+		// (a) determine the plaintext's length
+		//int ptextlen = findLength();
+		//System.out.println("plaintext length = "+ptextlen);
 
-	//=================== TASK 1 ======================//
-	for (int j = 0; j < 100; j++) {
+		// (b) determine a (rough) IV function
+		for (int i = 0; i < 100; i++) {
+			System.out.print(System.currentTimeMillis()+"\t");
+			int length = callEncrypt(null,0,ciphertext);
+			for (int j = 0; j<8; j++) {
+				System.out.print(String.format("%02X ",ciphertext[j]));
+			}
+			System.out.println("");
+		}
 
-		int length=callEncrypt(null,0,ciphertext);			
-		for(int i=0; i < 8; i++) {
-			System.out.print(String.format("0x2 ",ciphertext[i]));
-		}		
-		System.out.println("");    
-	}
     }
-    
 
+    // determines the length of the plaintext by gradually increasing
+    // the size of an additional prefix we add, until a new block is 
+    // created in the ciphertext
+    static int findLength() throws Exception {
+    	byte[] ciphertext = new byte[1024];
+    	byte[] prefix = new byte[8];
+    	int i = 0;
+    	boolean same = true;
+    	int prevLen = callEncrypt(prefix,0,ciphertext); // init length without any prefix 
+    	while (i < 8 && same) {
+    		int len = callEncrypt(prefix,i,ciphertext);
+    		System.out.println("adding prefix of size "+i+" yields ciphertext length "+len);
+    		if (len > prevLen) {
+    			same = false;
+    		} else {
+    			prevLen = len;
+    			i++;
+    		}
+    	}
+    	if (!same) {
+    		return (callEncrypt(null,0,ciphertext)-(i-1));
+    	} else {
+    		return -1;
+    	}
+    }
+
+
+    // returns an initial vector based on the current system time
+    // where the returned vector is 8 bytes long
+    static byte[] getInitVector() {
+    	byte[] iv = new byte[8];
+    	// ...
+    	return iv;
+    }
 
     // a helper method to call the external programme "encrypt" in the current directory
     // the parameters are the plaintext, length of plaintext, and ciphertext; returns length of ciphertext
